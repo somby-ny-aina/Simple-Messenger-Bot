3bb42584f39d19e29c58466af3e11724712b6a08
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-11-09 21:19:05 +0300

    Update ephoto.js

--- commands/ephoto.js
+++ commands/ephoto.js
@@ -10,7 +10,7 @@ module.exports = {
 
     try {
       const response = await axios.get(`https://e-photo.vercel.app/kshitiz`, { params: { text: text, number: number } });
-      const imageUrl = response.data.ing;
+      const imageUrl = response.data.result.ing;
 
       if (imageUrl) {
         await sendMessage(senderId, { attachment: { type: "image", payload: { url: imageUrl, is_reusable: true } } });
