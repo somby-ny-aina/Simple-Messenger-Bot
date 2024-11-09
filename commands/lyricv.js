4bf0d32c819db1bfa3d343c8868a4c8b56c3a2a9
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-11-09 22:38:37 +0300

    Create lyricv.js

---
+++ commands/lyricv.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+module.exports = {
+  execute: async (prompt, senderId, sendMessage) => {
+    if (!prompt) {
+      return sendMessage(senderId, { text: "❌ Please provide a search query after /lyricvideo." });
+    }
+
+    try {
+      const response = await axios.get(`https://lyric-search-neon.vercel.app/kshitiz`, { params: { keyword: query } });
+      const videos = response.data;
+
+      if (videos && Array.isArray(videos) && videos.length > 0) {
+        const selectedVideo = videos[Math.floor(Math.random() * videos.length)];
+        
+        const { videoUrl } = selectedVideo;
+
+        await sendMessage(senderId, {
+          text: `Enjoy the video!`,
+          attachment: { type: "video", payload: { url: videoUrl, is_reusable: true } }
+        });
+      } else {
+        sendMessage(senderId, { text: `❌ No videos found for "${query}".` });
+      }
+    } catch (error) {
+      console.error("Error fetching video:", error.message);
+      sendMessage(senderId, { text: "❌ Error fetching video." });
+    }
+  }
+};
