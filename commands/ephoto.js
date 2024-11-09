96960145973fc6e076dbc4f2923ce08a59ac315f
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-11-09 21:18:20 +0300

    Update ephoto.js

--- commands/ephoto.js
+++ commands/ephoto.js
@@ -4,13 +4,12 @@ module.exports = {
   execute: async (prompt, senderId, sendMessage) => {
     if (!prompt) return sendMessage(senderId, { text: "❌ Please provide a prompt after /ephoto." });
 
-    // Split the prompt by '|' to get the text and the number
-    const [text, number] = prompt.split('|').map(item => item.trim());
+    const [text, number] = prompt.split(' | ').map(item => item.trim());
 
     if (!text || !number) return sendMessage(senderId, { text: "❌ Please provide both a prompt and a number after /ephoto." });
 
     try {
-      const response = await axios.get(`https://e-photo.vercel.app/kshitiz`, { params: { text, number } });
+      const response = await axios.get(`https://e-photo.vercel.app/kshitiz`, { params: { text: text, number: number } });
       const imageUrl = response.data.ing;
 
       if (imageUrl) {
