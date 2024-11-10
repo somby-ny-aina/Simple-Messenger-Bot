221c4a922542dbe58f0756a7a323aa8b8f915358
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-11-10 22:17:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const handleMessage = async (event) => {
 
   if (message.toLowerCase() === 'help') {
     const commandList = Object.keys(commands).map(cmd => `┃➠ /${cmd}`).join('\n');
-    const helpMessage = `╭─〘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 〙─❍\n${commandList}\n╰─────〘${commandCount} 〙────❍`;
+    const helpMessage = `╭─〘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 〙─❍\n${commandList}\n╰──〘${commandCount} 〙─❍`;
     return sendMessage(senderID, { text: helpMessage });
   }
 
