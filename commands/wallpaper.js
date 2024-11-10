e8da8147782aaf88044243fe02a7850a52807efa
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-11-10 18:18:13 +0300

    Delete commands/wallpaper.js

--- commands/wallpaper.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-
-module.exports = {
-  execute: async (args, senderId, sendMessage) => {
-    const [query, page] = args.split("|").map(arg => arg.trim());
-    
-    if (!query || !page) {
-      return sendMessage(senderId, { text: "❌ Please provide both a search query and a page number, e.g., /wallpaper batman
