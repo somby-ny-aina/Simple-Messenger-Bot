66b7ced8a10c66a33b2df7ba19c3708bb7c10af2
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-11-10 10:01:56 +0300

    Update snoop.js

--- commands/snoop.js
+++ commands/snoop.js
@@ -4,8 +4,7 @@ module.exports = {
   execute: async (prompt, senderId, sendMessage) => {
     if (!prompt) return sendMessage(senderId, { text: "❌ Please provide a prompt after /snoop." });
 
-      const response = `https://sandipbaruwal.onrender.com/snoop?text=${encodeURIComponent(prompt)}`;
-      const imageUrl = response.data;
+      const imageUrl = `https://sandipbaruwal.onrender.com/snoop?text=${encodeURIComponent(prompt)}`;
 
       sendMessage(senderId, { attachment: { type: "audio", payload: { url: imageUrl, is_reusable: true } } });
   }
