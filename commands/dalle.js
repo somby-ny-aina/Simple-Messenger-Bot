f3223702294d663b469da590c3bb60893047d484
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-11-12 23:19:53 +0300

    Update dalle.js

--- commands/dalle.js
+++ commands/dalle.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+const sharp = require("sharp");
+const fs = require("fs");
 
 module.exports = {
   execute: async (prompt, senderId, sendMessage) => {
@@ -14,17 +16,21 @@ module.exports = {
       const data = response.data;
 
       if (data.status === "success" && data.data.status === "completed") {
-        const webpUrl = data.data.images[0];
-        
-        const conversionResponse = await axios.get(`https://ezgif.com/webp-to-jpg?url=${webpUrl}`, {
-          responseType: "arraybuffer"
-        });
+        const imageUr = data.data.images[0];
 
-        const jpgUrl = conversionResponse.data.result_url;
+        const imageResponse = await axios.get(imageUr, { responseType: "arraybuffer" });
+        const imageBuffer = Buffer.from(imageResponse.data, "binary");
+
+        const jpgBuffer = await sharp(imageBuffer).toFormat("jpg").toBuffer();
+
+        const tempFilePath = "./temp_image.jpg";
+        fs.writeFileSync(tempFilePath, jpgBuffer);
 
         await sendMessage(senderId, {
-          attachment: { type: "image", payload: { url: jpgUrl, is_reusable: true } }
+          attachment: { type: "image", payload: { url: tempFilePath, is_reusable: true } }
         });
+
+        fs.unlinkSync(tempFilePath);
       } else {
         sendMessage(senderId, { text: "❌ Failed to generate image." });
       }
