3062c0be0bc5da8007781e3492564a03c9cb6832
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-11-17 16:47:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PAGE_ACCESS_TOKEN = process.env.token;
 
 const sendMessage = async (senderId, message) => {
   try {
-    const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+    const PAGE_ACCESS_TOKEN = process.env.token;
     if (!PAGE_ACCESS_TOKEN) {
       throw new Error("PAGE_ACCESS_TOKEN is not set in the environment variables.");
     }
