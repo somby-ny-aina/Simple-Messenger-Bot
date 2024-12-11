4863bb287030f05f9cf67a999f8966a00c7c3600
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-12-11 17:13:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PAGE_ACCESS_TOKEN = process.env.token;
 
 const chatGpt = async (text, senderId) => {
   try {
-    const response = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o-pro?q=${encodeURIComponent(text)}&uid=${senderId}`);
+    const response = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?q=${encodeURIComponent(text)}&uid=${senderId}`);
 
     const reply = response.data.response;
 
