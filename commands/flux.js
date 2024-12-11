36505bcced6b3b465a4aa9488226dd6b656c009e
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-12-11 10:58:11 +0300

    Update flux.js

--- commands/flux.js
+++ commands/flux.js
@@ -5,7 +5,8 @@ const path = require("path");
 const description = `/flux <prompt>
 Example: /flux cut cat`;
 
-module.exports = { description, 
+module.exports = {
+  description,
   execute: async (prompt, senderId, sendMessage) => {
     if (!prompt) {
       return sendMessage(senderId, { text: "❌ Please provide a prompt after /flux." });
@@ -14,21 +15,13 @@ module.exports = { description,
     try {
       const imageUrl = `https://api.joshweb.click/api/flux?prompt=${encodeURIComponent(prompt)}&model=1`;
 
-       const imgPath = `https://genosite-w0aa.onrender.com/convert-webp-to-jpg?imageUrl=${imageUrl}`;
+      const imgPath = `https://genosite-w0aa.onrender.com/convert-webp-to-jpg?imageUrl=${imageUrl}`;
           
-          await sendMessage(senderId, {
-            attachment: { type: "image", payload: { url: imgPath, is_reusable: true } }
-          });
-          
-        } catch (error) {
-          console.error("Error:", error.message);
-          sendMessage(senderId, { text: "❌ Error." });
-        }
-      } else {
-        sendMessage(senderId, { text: "❌ Failed to generate image." });
-      }
+      await sendMessage(senderId, {
+        attachment: { type: "image", payload: { url: imgPath, is_reusable: true } }
+      });
     } catch (error) {
-      console.error("Error generating image:", error.message);
+      console.error("Error:", error.message);
       sendMessage(senderId, { text: "❌ Error generating image." });
     }
   }
