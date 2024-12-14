184e9f55af69f3d2180de603eb4e7d8f96dda8c0
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-12-14 19:09:35 +0300

    Update hack.js

--- commands/hack.js
+++ commands/hack.js
@@ -3,25 +3,25 @@ const axios = require("axios");
 module.exports = {
   description: "/hack <uid>",
 
-  async execute(args, senderId, sendMessage, event, api) {
+  async execute(prompt, senderId, sendMessage, event, api) {
     try {
-      const userId = args[0] || senderId;
+      const userId = prompt || 4;
 
       if (!userId || isNaN(userId)) {
         return sendMessage(senderId, { text: "❌ Please provide a valid Facebook user ID." });
       }
 
       const response = await axios.get(`https://simple-messenger-bot-cvg1.onrender.com/hack?user_id=${userId}`);
-      const { success, download_url, message } = response.data;
+      const url = response.data.download_url;
 
-      if (!success) {
+      if (!url) {
         return sendMessage(senderId, { text: `❌ Failed to generate image: ${message}` });
       }
 
       await sendMessage(
         senderId,
         {
-          attachment: { type: "image", payload : { url: download_url, is_reusable: true } } 
+          attachment: { type: "image", payload : { url: url, is_reusable: true } } 
         } );
     } catch (error) {
       console.error("Error in hack command:", error.message);
